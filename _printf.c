f175ea33101e8bc351eb2afcb4def852d25b407c
Mahmoud Mohamed <mahmoudmohamedhassann@gmail.com>
2023-11-15 00:27:58 -0800

    modified null string

--- _printf.c
+++ _printf.c
@@ -18,29 +18,26 @@ int _printf(const char *format, ...)
 		while (*format)
 		{
 			if (*format != '%')
-			{
-				write(1, format, 1);
-						no_of_char++;
-			}
+			{write(1, format, 1);
+			no_of_char++; }
 			else
 			{
 				format++;
 				if (*format == '%')
-				{
-					write(1, format, 1);
-						no_of_char++;
-				}
+				{write(1, format, 1);
+				no_of_char++; }
 				else if (*format == 'c')
-				{
-					char letter = va_arg(arguments_list, int);
+				{char letter = va_arg(arguments_list, int);
 						write(1, &letter, 1);
-							no_of_char++;
-				}
+							no_of_char++; }
 				else if (*format == 's')
-				{
-					char *ptrToString = va_arg(arguments_list, char *);
-						write(1, ptrToString, strlen(ptrToString));
-							no_of_char += strlen(ptrToString);
+				{char *ptrToString = va_arg(arguments_list, char *);
+					if (ptrToString == NULL)
+					{write(1, "(null)", 6);
+						no_of_char += 6; }
+					else
+					{write(1, ptrToString, strlen(ptrToString));
+							no_of_char += strlen(ptrToString); }
 				}
 			}
 			format++;
