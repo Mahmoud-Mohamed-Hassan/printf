3b104f63bb9620c6c3d0f7e40a0e8d3f5bdce13d
ManarAbdelhaliem <134805382+ManarAbdelhaliem@users.noreply.github.com>
2023-11-15 22:05:13 +0200

    Add files via upload

---
+++ print_numbers.c
@@ -0,0 +1,72 @@
+#include "main.h"
+int print_numbers_i(va_list v)
+{
+    int n = va_arg(v, int);
+    int number, last_number = n % 10, digit, x = 1;
+    int i = 1;
+    n = n / 10;
+    number = n;
+    if (last_number < 0)
+    {
+        _putchar('-');
+        number = -number;
+        n = -n;
+        last_number = -last_number;
+        i++;
+    }
+    if (number > 0)
+    {
+        while (number / 10 != 0)
+        {
+            x = x * 10;
+            number = number / 10;
+        }
+        number = n;
+        while (x > 0)
+        {
+            digit = number / x;
+            _putchar(digit + '0');
+            number = number - (digit * x);
+            x = x / 10;
+            i++;
+        }
+    }
+    _putchar(last_number + '0');
+    return (i);
+}
+int print_numbers_d(va_list v)
+{
+    int n =va_arg(v,int);
+    int number,last_number = n %10,digit,x=1;
+    int i =1;
+    n=n/10;
+    number=n;
+
+if(last_number<0)
+{
+    _putchar('-');
+    number = -number;
+    n = -n;
+    last_number=-last_number;
+    i++;
+}
+if (number >0)
+{
+    while (number / 10 != 0)
+    {
+        x=x *10;
+        number =number /10;
+    }
+    number =n;
+    while(x>0)
+    {
+        digit = number / x;
+        _putchar(digit + '0');
+        number=number -(digit * x);
+        x =x /10;
+        i++;
+    }
+}
+_putchar(last_number+'0');
+return (i);
+}
