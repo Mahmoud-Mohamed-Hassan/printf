d959a5c0a55bc59c1402ffa6f715ad6a3be99ddf
Mahmoud Mohamed <mahmoudmohamedhassann@gmail.com>
2023-11-14 23:24:55 -0800

    modified printf

--- _printf.c
+++ _printf.c
@@ -1,52 +1,50 @@
-#include "main.h"
-
 /**
- * _printf - prints anything
+ * _printf - prints %c,%s,%
  * @format: the format string
  *
  * Return: number of bytes printed
  */
+
+#include "main.h"
 
 int _printf(const char *format, ...)
 {
 	int no_of_char = 0;
 	va_list arguments_list;
 
-	if (format == NULL)
+	if (format == '\0')
 		return (-1);
-	va_start(arguments_list, format);
-	while (*format)
-	{
-		if (*format != '%')
+	va_start(arguments_list, format)
+		while (*format)
 		{
-			write(1, format, 1);
-			no_of_char++;
+			if (*format != '%')
+			{
+				write(1, format, 1);
+						no_of_char++;
+			}
+			else
+			{
+				format++;
+				if (*format == '%')
+				{
+					write(1, format, 1;)
+						no_of_char++;
+				}
+				else if (*format == 'c')
+				{
+					char letter = va_arg(arguments_list, int);
+						write(1, &letter, 1);
+							no_of_char++;
+				}
+				else if (*format == 's')
+				{
+					char *ptrToString = va_arg(argumentslist, char *);
+						write(1, ptrToString, strlen(ptrToString));
+							no_of_char += strlen(ptrToString);
+				}
+			}
+			format++;
 		}
-		else
-			format = format + 1;
-		if (*format == '\0')
-			break;
-		if (*format == '%')
-		{
-			write(1, format, 1);
-			no_of_char++;
-		}
-		else if (*format == 'c')
-		{
-			char c = va_arg(arguments_list, int);
-
-			write(1, &c, 1);
-			no_of_char++;
-		}
-		else if (*format == 's')
-		{
-			char *str = va_arg(arguments_list, char*);
-
-			write(1, str, strlen(str));
-			no_of_char += strlen(str);
-		}
-	}
-	format++;
 	va_end(arguments_list);
 	return (no_of_char);
 }
